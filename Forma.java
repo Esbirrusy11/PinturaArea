a65bbd24ebff9672db664baca731ad20e3a93995
Esbirrusy11 <raumarpar@alu.edu.gva.es>
2025-03-21 09:42:38 +0100

    Add files via upload

---
+++ Forma.java
@@ -0,0 +1,14 @@
+public class Forma {
+    String nombre;
+
+    public double area(){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Forma{" +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
+}
